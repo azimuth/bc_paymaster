f0860b44b5feff2f9f03c39425860c191ed5ff00
Christopher Maujean <cmaujean@gmail.com>
2010-05-06 17:03:36 -0700

    adding total hours to output

--- calc.rb
+++ calc.rb
@@ -3,10 +3,13 @@ require 'money'
 
 entries = TimeEntry.load_csv "time-report.csv"
 totalvalue = Money.us_dollar 0
+totalhours = 0
 
 entries.each do |e|
   totalvalue += e.value
+  totalhours += e.hours
   puts "#{e.hours} at #{e.rate.format}/hour #{e.rate_type} for #{e.company} is #{e.value.format}"
 end
 
 puts "Total monetary value for this report is: #{totalvalue.format}"
+puts "Total hours for this report is: #{totalhours}"
